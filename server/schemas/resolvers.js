d99c0cabbb063ecb12c472a76a5cf3d3c3087ac5
Amanda Dell <amhernandez1163@gmail.com>
2022-07-08 15:17:36 -0500

    updated resolvers actually this time and npm i concurrently

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,71 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in!");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError(
+          "You shall not pass! Invalid credentials"
+        );
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError(
+          "You shall not pass! Invalid credentials"
+        );
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveCharacter: async (parent, character, context) => {
+      if (context.user) {
+        console.log(character);
+        const updateUser = await User.findOneAndUpdate(
+          {
+            _id: context.user._id,
+          },
+          {
+            $addToSet: { savedCharacters: character },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+        console.log(updateUser);
+        return updateUser;
+      }
+      throw new AuthenticationError("Puny God, you need to be logged in.");
+    },
+
+    // removeCharacter: async (parent, )
+  },
+};
+
+module.exports = resolvers;
