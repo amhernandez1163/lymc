80874434112d6c31b6c8de7ad9ac008a851eb8d1
Amanda Dell <amhernandez1163@gmail.com>
2022-07-07 23:32:34 -0500

    updated typeDefs and index.js in schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    characterCount: Int
+    savedCharacters: [Characters]
+}
+
+type Characters {
+    characterId: ID
+    name: String
+    description: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(useranme: String!, email: String!, password: String!): Auth
+    saveCharacter(name: String!, characterId: String!, image: String!)
+    removeCharacter (characterId: ID): User
+}
+
+`;
+
+module.exports = typeDefs;
