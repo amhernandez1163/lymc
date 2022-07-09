0f02df1f4dd40783b4be11aa373104601f5df842
Cody Murray <cdillon.murray@yahoo.com>
2022-07-09 09:43:15 -0500

    updated pull

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,7 +4,7 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      user {
+      user: {
         _id
         username
         email
@@ -14,10 +14,10 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password String!) {
+mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
         token
-        user {
+        user: {
             _id
             username
             email
