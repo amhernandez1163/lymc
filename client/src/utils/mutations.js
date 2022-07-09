fd8bdb46845b0609270e1cdd4e567f348b356794
Amanda Dell <amhernandez1163@gmail.com>
2022-07-08 20:30:28 -0500

    deploying x 4

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -14,14 +14,25 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: $String!, $password String!) {
+mutation addUser($username: String!, $email: String!, $password String!) {
     addUser(username: $username, email: $email, password: $password) {
         token
         user {
             _id
             username
             email
+            characterCount
+            savedCharacters {
+                name
+                description
+                characterId
+                image
+            }
         }
     }
 }
 `;
+
+// export const SAVE_CHARACTER = gql`
+// mutation saveCharacter($)
+// `;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,6 @@ type Mutation {
     login(email: String!, password: String!): Auth
     addUser(useranme: String!, email: String!, password: String!): Auth
     saveCharacter(name: String!, characterId: String!, image: String!)
-    removeCharacter(characterId: ID): User
 }
 
 `;
