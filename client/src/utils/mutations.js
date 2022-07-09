a6c9d9e5c0a296ec05facb3ee68a5205bcdaf5b4
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-07-09 10:16:32 -0500

    pulling new code

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,10 +14,10 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password String!) {
+mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
         token
-        user {
+        user: {
             _id
             username
             email
