b9df076999284698a8e850339941d21dc311d679
Amanda Dell <amhernandez1163@gmail.com>
2022-07-10 09:29:48 -0500

    added model def to Character.js

--- server/models/Character.js
+++ server/models/Character.js
@@ -1,4 +1,4 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 //use for the User's saveChararcter array in User.js
 
@@ -19,5 +19,5 @@ const characterSchema = new Schema({
   },
 });
 
-// const Character = model("Character", characterSchema);
+const Character = model("Character", characterSchema);
 module.exports = characterSchema;
