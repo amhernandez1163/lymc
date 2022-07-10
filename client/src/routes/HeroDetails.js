b3df67bc288432191164b3cc3d3a70534944ccfd
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-07-10 14:31:48 -0500

    resolve character id issue

--- client/src/routes/HeroDetails.js
+++ client/src/routes/HeroDetails.js
@@ -6,49 +6,56 @@ import { fetchHero } from "../utils/utils";
 
 export default function HeroDetails() {
 	const { id } = useParams();
-
 	console.log("id from url", id);
-
 	const [hero, setHero] = useState();
 
+	const getData = async () => {
+		if (id) {
+			fetchHero(id)
+				.then(async (data) => {
+					console.log("api data response ===", data);
+					await setHero(data.data.results[0]);
+				})
+				.catch((err) => console.error(err));
+		}
+	};
+
 	useEffect(() => {
-		fetchHero(id)
-			.then((data) => {
-				console.log(data);
-				setHero(data.data.results[0]);
-			})
-			.catch((err) => console.error(err));
+		getData();
 	}, []);
 
-	if (!hero) return; // If hero is null, undefined, or falsy, return
-	console.log("hero", hero);
-
 	return (
-		<div className="container large">
-			<div className="hero_details-container">
-				<img
-					src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
-					alt="hero image full size"
-				/>
-				<div className="hero_details">
-					<h4>Name</h4>
-					<p>{hero.name}</p>
-					{hero.description ? (
-						<>
-							<h4>Description</h4>
-							<p>{hero.description}</p>
-						</>
-					) : null}
-					<div>
-						<h4>Series</h4>
-						<ul>
-							{hero.series.items.map((s) => (
-								<li key={Math.random() * 1000}>{s.name}</li>
-							))}
-						</ul>
+		<>
+			{hero ? (
+				<div className="container large">
+					<div className="hero_details-container">
+						<img
+							src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+							alt="Marvel Character"
+						/>
+						<div className="hero_details">
+							<h4>Name</h4>
+							<p>{hero.name}</p>
+							{hero.description ? (
+								<>
+									<h4>Description</h4>
+									<p>{hero.description}</p>
+								</>
+							) : null}
+							<div>
+								<h4>Series</h4>
+								<ul>
+									{hero.series.items.map((s) => (
+										<li key={Math.random() * 1000}>{s.name}</li>
+									))}
+								</ul>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			) : (
+				<></>
+			)}
+		</>
 	);
 }
