7eed544ae3f66670ddcc1be2d7592724af9efb1e
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-07-11 16:16:57 -0500

    trying to fix saveCharacters

--- client/src/pages/saveCharacters.js
+++ client/src/pages/saveCharacters.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from "react";
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { GET_ME } from '../utils/queries';
 import{ REMOVE_CHARACTER } from '../utils/mutations';
@@ -11,7 +12,7 @@ const SavedCharacter = () => {
   const {data} = useQuery (GET_ME)
   const userData = data?.me || {}
   console.log (data, userData)
-  const userDataLenght = Object.keys(userData).length;
+  // const userDataLength = Object.keys(userData).length;
 
   const handleDeleteCharacters = async (characterId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -51,7 +52,7 @@ const SavedCharacter = () => {
                 {character.image ? <Card.Img src={character.image} alt={`The cover for ${character.title}`} variant='top' /> : null}
                 <Card.Body>
                   <Card.Title>{character.name}</Card.Title>
-                  {/* <p className='small'>Characters: {book.author}</p> */}
+                  <p className='small'>Characters: {character.series}</p>
                   <Card.Text>{character.description}</Card.Text>
                   <Button className='btn-block btn-danger' onClick={() => handleDeleteCharacters(character.characterId)}>
                     Delete this Character!
