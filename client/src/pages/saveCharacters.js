b50132980c8ee0ae3c87aa33eee4c137eb967a54
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-07-11 15:38:06 -0500

    pulling code

--- client/src/pages/saveCharacters.js
+++ client/src/pages/saveCharacters.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { GET_ME } from '../utils/queries';
-import{ REMOVE_BOOK } from '../utils/mutations';
+import{ REMOVE_CHARACTER } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import { removeCharacterId } from '../utils/localStorage';
 import { useMutation, useQuery } from '@apollo/client';
 
-const SaveBook = () => {
-  const [removeCharacter] = useMutation (REMOVE_BOOK)
+const SavedCharacter = () => {
+  const [removeCharacter] = useMutation (REMOVE_CHARACTER)
   const {data} = useQuery (GET_ME)
   const userData = data?.me || {}
   console.log (data, userData)
@@ -24,10 +25,45 @@ const SaveBook = () => {
         variables: {characterId: characterId}        
       })
 
-      removeBookId(characterId);
+      removeCharacterId(characterId);
     }catch (err) {
       console.error(err);
     }
   }; 
-  return )
-}
+
+  return (
+    <>
+    <Jumbotron fluid className='text-light bg-dark'>
+     <Container>
+      <h1>Viewing saved characters!</h1>
+      </Container> 
+    </Jumbotron>
+    <Container>
+      <h2>
+      {userData?.SavedCharater?.length
+            ? `Viewing ${userData.savedCharater.length} saved ${userData.savedCharater.length === 1 ? 'character' : 'characters'}:`
+            : 'You have no saved character!'}
+      </h2>
+      <CardColumns>
+      {userData?.savedCharacters?.map((character) => {
+            return (
+              <Card key={character.characterId} border='dark'>
+                {character.image ? <Card.Img src={character.image} alt={`The cover for ${character.title}`} variant='top' /> : null}
+                <Card.Body>
+                  <Card.Title>{character.name}</Card.Title>
+                  {/* <p className='small'>Characters: {book.author}</p> */}
+                  <Card.Text>{character.description}</Card.Text>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteCharacters(character.characterId)}>
+                    Delete this Character!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedCharacter;
