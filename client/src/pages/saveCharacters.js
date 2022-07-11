83e9e43bc6f640ec3b17232dee3a6e2861065c9a
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-07-10 22:13:33 -0500

    coding updates

--- client/src/pages/saveCharacters.js
+++ client/src/pages/saveCharacters.js
@@ -5,3 +5,28 @@ import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import { useMutation, useQuery } from '@apollo/client';
 
+const SaveBook = () => {
+  const [removebook] = useMutation (REMOVE_BOOK)
+  const {data} = useQuery (GET_ME)
+  const userData = data?.me || {}
+  console.log (data, userData)
+  const userDataLenght = Object.keys(userData).length;
+
+  const handleDeleteCharaters = async (characterId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+    
+    try {
+      const {data} = await removeBook ({
+        variables: {bookId: bookId}        
+      })
+
+      removeBookId(bookId);
+    }catch (err) {
+      console.error(err);
+    }
+  }; 
+}
