a1f9e6b2e664bd6fea750fae6b80692ab1981227
Amanda Dell <amhernandez1163@gmail.com>
2022-07-12 18:44:54 -0500

    npm i dotenv

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -3,53 +3,53 @@ import MD5 from "crypto-js/md5";
 const API_URL = process.env.REACT_APP_BASE_URL;
 
 const getHash = (ts, privateKey, apiKey) => {
-	return MD5(ts + privateKey + apiKey).toString();
+  return MD5(ts + privateKey + apiKey).toString();
 };
 
 const fetchCharacters = async (value) => {
-	// get api from docs
-	let baseUrl = `${API_URL}/v1/public/characters`;
+  // get api from docs
+  let baseUrl = `${API_URL}/v1/public/characters`;
 
-	let ts = Date.now().toString();
-	let apiKey = process.env.REACT_APP_API_KEY;
-	let privateKey = process.env.REACT_APP_PRIVATE_KEY;
+  let ts = Date.now().toString();
+  let apiKey = process.env.REACT_APP_API_KEY;
+  let privateKey = process.env.REACT_APP_PRIVATE_KEY;
 
-	let hash = getHash(ts, privateKey, apiKey);
+  let hash = getHash(ts, privateKey, apiKey);
 
-	let url = `${baseUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}&nameStartsWith=${value}`;
+  let url = `${baseUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}&nameStartsWith=${value}`;
 
-	try {
-		let response = await fetch(url);
-		let data = await response.json();
-		console.log(data.data.results);
-		return data.data.results;
-	} catch (err) {
-		console.error(err);
-		return;
-	}
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data.data.results);
+    return data.data.results;
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 };
 
 const fetchCharacter = async (id) => {
-	// get api from docs
-	let baseUrl = `${API_URL}/v1/public/characters/${id}`;
+  // get api from docs
+  let baseUrl = `${API_URL}/v1/public/characters/${id}`;
 
-	let ts = Date.now().toString();
-	let apiKey = process.env.REACT_APP_API_KEY;
-	let privateKey = process.env.REACT_APP_PRIVATE_KEY;
+  let ts = Date.now().toString();
+  let apiKey = process.env.REACT_APP_API_KEY;
+  let privateKey = process.env.REACT_APP_PRIVATE_KEY;
 
-	let hash = getHash(ts, privateKey, apiKey);
+  let hash = getHash(ts, privateKey, apiKey);
 
-	let url = `${baseUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}`;
+  let url = `${baseUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}`;
 
-	try {
-		let response = await fetch(url);
-		let data = await response.json();
-		console.log(data);
-		return data;
-	} catch (err) {
-		console.error(err);
-		return;
-	}
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 };
 
 export { fetchCharacters, fetchCharacter };
