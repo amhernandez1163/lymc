354b120f8c75ad0eb604b4504d352040622c0ec7
Cody Murray <cdillon.murray@yahoo.com>
2022-07-12 21:22:52 -0500

    updated readme

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,62 +3,58 @@ import { Flex, Heading, HStack, Link } from "@chakra-ui/react";
 import Music from "../routes/Music";
 
 export default function Footer() {
-  return (
-    <footer className="footer fixed-bottom">
-      <Flex
-        p={2}
-        backgroundColor={"black"}
-        as="nav"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Heading color={"#ffffff8c"} as="h1" size="md">
-          Data provided by Marvel. © 2014 Marvel{" "}
-        </Heading>
-        <HStack color={"white"} fontSize="1.5xl" spacing={6}>
-          <Link
-            a
-            href="https://github.com/amhernandez1163"
-            target="_blank"
-            rel="noreferrer noopener"
-            fontSize={"2xl"}
-            textDecoration={"underline"}
-          >
-            Amanda Hernandez
-          </Link>
-          <Link
-            a
-            href="https://github.com/EvelynAguirreOrtiz"
-            target="_blank"
-            rel="noreferrer noopener"
-            fontSize={"2xl"}
-            textDecoration={"underline"}
-          >
-            Evelyn Aguirre
-          </Link>
-          <Link
-            a
-            href="https://github.com/Bluebear1701"
-            target="_blank"
-            rel="noreferrer noopener"
-            fontSize={"2xl"}
-            textDecoration={"underline"}
-          >
-            Monica Solano
-          </Link>
-          <Link
-            a
-            href="https://github.com/CDMurr"
-            target="_blank"
-            rel="noreferrer noopener"
-            fontSize={"2xl"}
-            textDecoration={"underline"}
-          >
-            Cody Murray
-          </Link>
-          <Music />
-        </HStack>
-      </Flex>
-    </footer>
-  );
+    return (
+        <footer className="footer fixed-bottom">
+            <Flex
+                p={2}
+                backgroundColor={"black"}
+                as="nav"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Heading color={"#ffffff8c"} as="h1" size="xs">
+                    Data provided by Marvel. © 2014 Marvel{" "}
+                </Heading>
+                <HStack color={"white"} fontSize={9} spacing={6}>
+                    <Link
+                        a
+                        href="https://github.com/amhernandez1163"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        textDecoration={"underline"}
+                    >
+                        Amanda Hernandez
+                    </Link>
+                    <Link
+                        a
+                        href="https://github.com/EvelynAguirreOrtiz"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        textDecoration={"underline"}
+                    >
+                        Evelyn Aguirre
+                    </Link>
+                    <Link
+                        a
+                        href="https://github.com/Bluebear1701"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        textDecoration={"underline"}
+                    >
+                        Monica Solano
+                    </Link>
+                    <Link
+                        a
+                        href="https://github.com/CDMurr"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        textDecoration={"underline"}
+                    >
+                        Cody Murray
+                    </Link>
+                    <Music />
+                </HStack>
+            </Flex>
+        </footer>
+    );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,81 +8,102 @@ import { Heading, HStack } from "@chakra-ui/react";
 import Auth from "../utils/auth";
 
 const AppNavbar = () => {
-  // set modal display state
-  const [showModal, setShowModal] = useState(false);
+    // set modal display state
+    const [showModal, setShowModal] = useState(false);
 
-  return (
-    <>
-      {/* <Flex backgroundColor={'black'} as="nav" p={6} justifyContent="space-between" alignItems="center"> */}
-      <Navbar className="Navbar sticky-top" bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-          
-          <Navbar.Brand as={Link} to="/">
-          <Heading color={'#ffffff8c'} as="h1" size="3xl">LYMC</Heading>
-          </Navbar.Brand>
-      <HStack  fontSize="4xl" spacing={6}>
-          <Navbar.Toggle aria-controls="navbar" />
-          <Navbar.Collapse id="navbar">
-            <Nav className="ml-auto">
-              <Nav.Link className="link" as={Link} to="/">
-                Search the MCU
-              </Nav.Link>
-              {Auth.loggedIn() ? (
-                <>
-                  
-                  <Nav.Link className="link" as={Link} to="/saved">
-                    Saved Characters
-                  </Nav.Link>
-                  
-                  <Nav.Link className="link" onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link className="link" onClick={() => setShowModal(true)}>
-                  Login/Sign Up
-                </Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-      </HStack>
-        </Container>
-      </Navbar>
-      {/* set modal data up */}
-      <Modal
-        size="lg"
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby="signup-modal"
-      >
-        {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey="login">
-          <Modal.Header closeButton>
-            <Modal.Title id="signup-modal">
-              <Nav variant="pills">
-                <Nav.Item>
-                  <Nav.Link eventKey="login">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey="login">
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey="signup">
-                <SignupForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
-      
-    
-    </>
-  );
+    return (
+        <>
+            {/* <Flex backgroundColor={'black'} as="nav" p={6} justifyContent="space-between" alignItems="center"> */}
+            <Navbar
+                className="Navbar sticky-top"
+                bg="dark"
+                variant="dark"
+                expand="lg"
+            >
+                <Container fluid>
+                    <Navbar.Brand as={Link} to="/">
+                        <Heading color={"#ffffff8c"} as="h1" size="3xl">
+                            LYMC
+                        </Heading>
+                    </Navbar.Brand>
+                    <HStack fontSize="1xl" spacing={6}>
+                        <Navbar.Toggle aria-controls="navbar" />
+                        <Navbar.Collapse id="navbar">
+                            <Nav className="ml-auto">
+                                <Nav.Link className="link" as={Link} to="/">
+                                    Search the MCU
+                                </Nav.Link>
+                                {Auth.loggedIn() ? (
+                                    <>
+                                        <Nav.Link
+                                            className="link"
+                                            as={Link}
+                                            to="/saved"
+                                        >
+                                            Saved Characters
+                                        </Nav.Link>
+
+                                        <Nav.Link
+                                            className="link"
+                                            onClick={Auth.logout}
+                                        >
+                                            Logout
+                                        </Nav.Link>
+                                    </>
+                                ) : (
+                                    <Nav.Link
+                                        className="link"
+                                        onClick={() => setShowModal(true)}
+                                    >
+                                        Login/Sign Up
+                                    </Nav.Link>
+                                )}
+                            </Nav>
+                        </Navbar.Collapse>
+                    </HStack>
+                </Container>
+            </Navbar>
+            {/* set modal data up */}
+            <Modal
+                size="lg"
+                show={showModal}
+                onHide={() => setShowModal(false)}
+                aria-labelledby="signup-modal"
+            >
+                {/* tab container to do either signup or login component */}
+                <Tab.Container defaultActiveKey="login">
+                    <Modal.Header closeButton>
+                        <Modal.Title id="signup-modal">
+                            <Nav variant="pills">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="login">Login</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="signup">
+                                        Sign Up
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="login">
+                                <LoginForm
+                                    handleModalClose={() => setShowModal(false)}
+                                />
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="signup">
+                                <SignupForm
+                                    handleModalClose={() => setShowModal(false)}
+                                />
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Modal.Body>
+                </Tab.Container>
+            </Modal>
+        </>
+    );
 };
 
 export default AppNavbar;
