c247a82dd013623a1ef4503dc083a5b0b5fd2523
Amanda Dell <amhernandez1163@gmail.com>
2022-07-12 23:24:55 -0500

    updated footer from cody

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,35 +1,58 @@
 import React from "react";
 import { Flex, Heading, HStack, Link } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
 import Music from "../routes/Music";
 
 export default function Footer() {
   return (
     <footer className="footer fixed-bottom">
-      <Music />
       <Flex
-        backgroundColor={"gray"}
+        p={2}
+        backgroundColor={"black"}
         as="nav"
-        p={6}
         justifyContent="space-between"
         alignItems="center"
       >
-        <Heading as="h1" size="md">
+        <Heading color={"#ffffff8c"} as="h1" size="xs">
           Data provided by Marvel. © 2014 Marvel{" "}
         </Heading>
-        <HStack spacing={6}>
-          <Link as={RouterLink} to="https://github.com/CDMurr">
+        <HStack color={"white"} fontSize={9} spacing={6}>
+          <Link
+            a
+            href="https://github.com/amhernandez1163"
+            target="_blank"
+            rel="noreferrer noopener"
+            textDecoration={"underline"}
+          >
             Amanda Hernandez
           </Link>
-          <Link as={RouterLink} to="https://github.com/CDMurr">
+          <Link
+            a
+            href="https://github.com/EvelynAguirreOrtiz"
+            target="_blank"
+            rel="noreferrer noopener"
+            textDecoration={"underline"}
+          >
             Evelyn Aguirre
           </Link>
-          <Link as={RouterLink} to="https://github.com/CDMurr">
+          <Link
+            a
+            href="https://github.com/Bluebear1701"
+            target="_blank"
+            rel="noreferrer noopener"
+            textDecoration={"underline"}
+          >
             Monica Solano
           </Link>
-          <Link as={RouterLink} to="https://github.com/CDMurr">
+          <Link
+            a
+            href="https://github.com/CDMurr"
+            target="_blank"
+            rel="noreferrer noopener"
+            textDecoration={"underline"}
+          >
             Cody Murray
           </Link>
+          <Music />
         </HStack>
       </Flex>
     </footer>
